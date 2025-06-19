abdd54f6698d1f3deac3583f1179d515588d1145
R Sengupta <105560427+rxngxpta@users.noreply.github.com>
2025-06-20 04:38:44 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,8 +13,6 @@
 from yaml.loader import SafeLoader
 import os
 import streamlit as st
-st.set_page_config(page_title="Home | Ledgr", page_icon=None,
-                   layout="centered", initial_sidebar_state="expanded")
 direc = os.getcwd()
 
 ##################################################################
