2f0a9cd8130d75d69922f34f39154380bbd79799
R Sengupta <105560427+rxngxpta@users.noreply.github.com>
2025-07-20 02:05:31 +0530

    Update 7 - Your_Journey_and_Feedback.py

--- pages/7 - Your_Journey_and_Feedback.py
+++ pages/7 - Your_Journey_and_Feedback.py
@@ -5,7 +5,6 @@ import os
 
 st.set_page_config(page_title='Ledgr | Contact Us', layout="wide",
                    initial_sidebar_state="expanded")
-st.html("<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7543848340955227" crossorigin="anonymous"></script>", width="stretch")
 
 
 direc = os.getcwd()
