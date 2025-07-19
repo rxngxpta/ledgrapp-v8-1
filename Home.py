98426984218d66f4342919d381d4d779616392d0
R Sengupta <105560427+rxngxpta@users.noreply.github.com>
2025-07-19 23:55:47 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,8 +16,7 @@ import streamlit as st
 # import html
 
 st.html(
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7543848340955227"
-     crossorigin="anonymous"></script>)
+    "<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7543848340955227" crossorigin="anonymous"></script>", width="stretch")
 
 direc = os.getcwd()
 
