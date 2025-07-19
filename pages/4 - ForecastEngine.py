28152138170f25e7721099ff47f0432614b2c87c
R Sengupta <105560427+rxngxpta@users.noreply.github.com>
2025-07-19 23:15:04 +0530

    Update 4 - ForecastEngine.py

--- pages/4 - ForecastEngine.py
+++ pages/4 - ForecastEngine.py
@@ -27,8 +27,8 @@ st.set_page_config(page_title='Ledgr | Forecasting Engine',
 import html
 
 st.html(
-    "<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7543848340955227"
-     crossorigin="anonymous"></script>")
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7543848340955227"
+     crossorigin="anonymous"></script>)
 
 def inject_css():
     with open("style.css") as f:
